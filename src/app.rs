d44db0db761a33817f826b13fc9ff507330d529a
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-10-15 12:50:11 +0200

    Thanks mgi388

--- src/app.rs
+++ src/app.rs
@@ -21,6 +21,10 @@ fn add_enemy(mut commands: Commands) {
 pub fn create_app() -> App {
     let mut app = App::new();
 
+    // The idea for the function 'try_add_plugins' 
+    // (https://github.com/bevyengine/bevy/discussions/15802#discussioncomment-10898148)
+    // will make this if obsolete and increase code coverage.
+    // Thanks mgi388 for pointing this out
     if cfg!(test) {
         app.add_plugins(TaskPoolPlugin::default());
         //app.add_plugins(AssetPlugin::default());
